3007d29b1edc9099556fb346fca3ef996e43789d
vincentDarcq <vincent.darcq@hotmail.fr>
2022-06-19 22:47:50 +0200

    add prod environment

--- server/environment/production.js
+++ server/environment/production.js
@@ -1,5 +1,7 @@
 module.exports = {
   dbUrl: "mongodb+srv://underrated:uTLhewpTC12@cluster0.51i9g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  cert: '/etc/letsencrypt/live/nderrated.fr/fullchain.pem',
+  key: '/etc/letsencrypt/live/nderrated.fr/privkey.pem',
   portHttp: 80,
   portHttps: 443
 }
