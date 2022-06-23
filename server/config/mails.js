74d3bf043bc0aab446461363ed4072b2ec01408e
vincentDarcq <vincent.darcq@hotmail.fr>
2022-06-23 09:03:51 +0200

    changes header color & mail

--- server/config/mails.js
+++ server/config/mails.js
@@ -4,24 +4,17 @@ const transporter = nodemailer.createTransport(sparkPostTransport({
   sparkPostApiKey: 'b58e0c8cdc97844c06e6e7e7f57eb4fe5df73067',
   host: 'https://api.eu.sparkpost.com/api/v1'
 }));
-const transport = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-      user: "vincent.darcq@hotmail.fr",
-      pass: "uTLhewpTC12;"
-  }
-});
 
 module.exports.sendMail = async (nom, mail, msg, response) => {
     const htmlToSend = '<div style="width: 700px">' + msg + '</div>'
   let message = {
-    to: 'vincent.darcq@hotmail.fr',
-    from: 'Nderrated <no_reply@eventmap.fr>',
+    to: 'contact@nderrated.fr',
+    from: 'Nderrated <contact@contact.nderrated.fr>',
     subject: 'Message de ' + mail,
     html: htmlToSend,
     text: 'Hello Team! <br><br>Please find attached...<br><br>Thanks,<br>XXXXX'
   };
-  await transport.sendMail(message).then((res, err) => {
+  await transporter.sendMail(message).then((res, err) => {
     if (err) { return err; }
     return response.status(200).json("mail envoyé");
   });
